3c2cc630f37b22e2d2e28615a214c6a41345389b
Goran Stevanović <stevanovicgoran@proton.me>
2026-03-27 15:28:20 +0100

    Update Auth Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,38 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { User } from 'src/users/entities/user.entity';
 import { AuthService } from './auth.service';
-import { PasswordValidationService } from './password-validation.service';
 import { AuthController } from './auth.controller';
+import { PasswordValidationService } from './password-validation.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtAuthGurad } from './guards/jwt-auth.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [AuthService, PasswordValidationService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRATION') || '24h',
+        },
+      }),
+    }),
+  ],
+  providers: [
+    AuthService,
+    PasswordValidationService,
+    JwtStrategy,
+    JwtAuthGurad,
+  ],
   controllers: [AuthController],
+  exports: [AuthService, JwtAuthGurad, JwtStrategy],
 })
 export class AuthModule {}
