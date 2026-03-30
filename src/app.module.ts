1b5835625363c81283ddb83bfde330f79917e876
z0bla <rn.blazana@gmail.com>
2026-03-30 17:08:00 +0200

    implement categories module and fix tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import databaseConfig from './config/database.config';
 import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
 import jwtConfig from './config/jwt.config';
 
 @Module({
   imports: [
-    // Load environment variables
     ConfigModule.forRoot({
       isGlobal: true,
       load: [databaseConfig, jwtConfig],
     }),
-    // Database connection
     DatabaseModule,
-    // Other modules will be added here
     AuthModule,
+    CategoriesModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
---
+++ src/categories/dto/create-category.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  icon: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+}
