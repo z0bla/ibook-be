dde13d2efcee24bca2bd9a998adb7abc5d34e811
Goran Stevanović <stevanovicgoran@proton.me>
2026-04-27 18:57:08 +0200

    Add method findAllWithSalonCount()

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -3,16 +3,33 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Category } from './entities/category.entity';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { Salon } from '../salons/entities/salon.entity';
 
 @Injectable()
 export class CategoriesService {
   constructor(
     @InjectRepository(Category)
     private readonly categoryRepository: Repository<Category>,
+    private readonly salonRepository: Repository<Salon>,
   ) {}
 
   async findAll(): Promise<Category[]> {
     return this.categoryRepository.find();
+  }
+
+  async findAllWithSalonCount() {
+    const categories = await this.categoryRepository.find();
+
+    const categoriesWithCount = await Promise.all(
+      categories.map(async (category) => {
+        const salonCount = await this.salonRepository.count({
+          where: { category: { id: category.id } },
+        });
+        return { ...category, salonCount };
+      }),
+    );
+
+    return categoriesWithCount;
   }
 
   async findById(id: string): Promise<Category> {
